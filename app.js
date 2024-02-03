ebbed2bb089dd5283d37819f8e98aa26b7039c2e
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2024-02-04 07:27:31 +0800

    wrote initital data on the database (15%)

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'))
 
-
 const app = express();
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user')
+
 router.get('/', (req,res) => res.render('index', {user: req.user}));
+
+
+//FOR SIGN UP
+router.get('/sign-up', (req,res) => res.render('sign-up-form'))
+
+router.post('/sign-up', async (req,res, next) => {
+    try {
+        const user = new User({
+            first_name: req.body.firstname,
+            family_name: req.body.lastname,
+            user_name: req.body.username,
+            password: req.body.password,
+        })
+        const result = await user.save();
+        res.redirect('/');
+    }catch(err){
+        return next(err)
+    }
+})
+
 
 module.exports = router;
